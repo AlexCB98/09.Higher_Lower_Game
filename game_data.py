0b63e986fc946bd80767fb2815eac6b45fd2cd92
Alexandru <lexyus22@gmail.com>
2026-05-01 12:01:07 +0200

    Added format_data function and basic test output.

---
+++ game_data.py
@@ -0,0 +1,182 @@
+data = [
+    {
+        'name': 'TikTok',
+        'follower_count': 380,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'MrBeast',
+        'follower_count': 250,
+        'description': 'YouTuber and entrepreneur',
+        'country': 'United States'
+    },
+    {
+        'name': 'Elon Musk',
+        'follower_count': 200,
+        'description': 'Entrepreneur and CEO of Tesla and SpaceX',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylian Mbappé',
+        'follower_count': 120,
+        'description': 'Footballer',
+        'country': 'France'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 185,
+        'description': 'Actress and fashion icon',
+        'country': 'United States'
+    },
+    {
+        'name': 'BLACKPINK',
+        'follower_count': 170,
+        'description': 'K-pop girl group',
+        'country': 'South Korea'
+    },
+    {
+        'name': 'Samsung',
+        'follower_count': 95,
+        'description': 'Technology company',
+        'country': 'South Korea'
+    },
+    {
+        'name': 'Apple',
+        'follower_count': 310,
+        'description': 'Technology company',
+        'country': 'United States'
+    },
+    {
+        'name': 'Netflix',
+        'follower_count': 260,
+        'description': 'Streaming platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'YouTube',
+        'follower_count': 300,
+        'description': 'Video sharing platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dua Lipa',
+        'follower_count': 88,
+        'description': 'Musician',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'The Weeknd',
+        'follower_count': 95,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Post Malone',
+        'follower_count': 75,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Keanu Reeves',
+        'follower_count': 60,
+        'description': 'Actor',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Henry Cavill',
+        'follower_count': 58,
+        'description': 'Actor',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Tom Holland',
+        'follower_count': 67,
+        'description': 'Actor',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Chris Hemsworth',
+        'follower_count': 65,
+        'description': 'Actor',
+        'country': 'Australia'
+    },
+    {
+        'name': 'BMW',
+        'follower_count': 72,
+        'description': 'Car manufacturer',
+        'country': 'Germany'
+    },
+    {
+        'name': 'Mercedes-Benz',
+        'follower_count': 76,
+        'description': 'Car manufacturer',
+        'country': 'Germany'
+    },
+    {
+        'name': 'PewDiePie',
+        'follower_count': 110,
+        'description': 'YouTuber',
+        'country': 'Sweden'
+    },
+    {
+        'name': 'Mark Zuckerberg',
+        'follower_count': 90,
+        'description': 'CEO of Meta',
+        'country': 'United States'
+    },
+    {
+        'name': 'OpenAI',
+        'follower_count': 85,
+        'description': 'AI research company',
+        'country': 'United States'
+    },
+    {
+        'name': 'Google',
+        'follower_count': 290,
+        'description': 'Technology company',
+        'country': 'United States'
+    },
+    {
+        'name': 'Amazon',
+        'follower_count': 280,
+        'description': 'E-commerce company',
+        'country': 'United States'
+    },
+    {
+        'name': 'Spotify',
+        'follower_count': 210,
+        'description': 'Music streaming platform',
+        'country': 'Sweden'
+    },
+    {
+        'name': 'Lewis Hamilton',
+        'follower_count': 62,
+        'description': 'Formula 1 driver',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Conor McGregor',
+        'follower_count': 68,
+        'description': 'MMA fighter',
+        'country': 'Ireland'
+    },
+    {
+        'name': 'Greta Thunberg',
+        'follower_count': 55,
+        'description': 'Climate activist',
+        'country': 'Sweden'
+    },
+    {
+        'name': 'Haaland',
+        'follower_count': 66,
+        'description': 'Footballer',
+        'country': 'Norway'
+    },
+    {
+        'name': 'Fortnite',
+        'follower_count': 92,
+        'description': 'Video game',
+        'country': 'United States'
+    }
+]
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
-from random import randint
+from random import choice
+from game_data import data
 
 print('\nWelcome to the * Higher - Lower * game.\n')
 
+def format_data(account):
+    name = account['name']
+    description = account['description']
+    country = account['country']
 
-data = [{
-        'name': 'Dwayne Johnson',
-        'follower_count': 181,
-        'description': 'Actor and professional wrestler',
-        'country': 'United States'
-},
-{
-        'name': 'Selena Gomez',
-        'follower_count': 174,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kylie Jenner',
-        'follower_count': 172,
-        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kim Kardashian',
-        'follower_count': 167,
-        'description': 'Reality TV personality and businesswoman',
-        'country': 'United States'
-    }]
+    return f'{name}, {description}, from {country}.'
 
-rnd_data = data[randint(0,3)]
-print(rnd_data)
-
+print(format_data(data[0]))

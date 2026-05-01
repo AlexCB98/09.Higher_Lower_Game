5068971854dc787cf888a7f22295f66f73be9592
Alexandru <lexyus22@gmail.com>
2026-05-01 13:36:01 +0200

    Add check_answer function to validate user guess.

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ def format_data(account):
 
     return f'{name}, {description}, from {country}.'
 
-print(format_data(data[0]))
+account_a = choice(data)
+account_b = choice(data)
+
+user_guess = input('Who have more followers?: Type "A" or "B": ').lower()
+
+def check_answer(guess, a_follower_count, b_follower_count):
+    if a_follower_count > b_follower_count:
+        answer = 'a'
+    else:
+        answer = 'b'
+    return guess == answer
